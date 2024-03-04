ad3fce6984335c9c9ca6f2f1fd00c945bb80cc4e
andrewKemp2319 <162205327+andrewKemp2319@users.noreply.github.com>
2024-03-04 16:45:11 -0500

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Andy/Documents/CollegeWork/Fall 2023/Team Projects 2/projectCode
+# Build directory: C:/Users/Andy/Documents/CollegeWork/Fall 2023/Team Projects 2/projectCode/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
